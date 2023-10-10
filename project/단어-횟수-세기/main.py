070b6ae4ae7f5630b6c1666fc32723d83d47a655
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2023-10-10 09:47:01 +0900

    Update and rename project/단어-횟수-세기.py to project/단어-횟수-세기/main.py

---
+++ project/단어-횟수-세기/main.py
@@ -0,0 +1,40 @@
+# 단어 횟수 세기 : 텍스트 파일에서 각 단어가 얼마나 자주 나오는지 세는 프로그램 작성
+#
+# 파일 : a.
+
+def openFile():
+
+    file = open("파일명.txt", 'r')
+
+    return file
+
+def closeFile(file):
+
+    file.close()
+
+def countWord(wordList):
+
+    dic = dict()
+    for word in wordList:
+
+        try: dic[word] = dic[word] + 1
+        except: dic[word] = 1
+
+    return dic
+
+def main():
+    
+    file = openFile()
+
+    context = file.read()
+    wordList = context.split()
+
+    dic = countWord(wordList)
+
+    print(dic)
+
+    closeFile(file)
+
+
+if __name__ == "__main__":
+    main()
